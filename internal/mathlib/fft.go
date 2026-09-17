27f6c534ff4307cefe10496d3f69ab870e8636b7
lorsan <stasstrochewskij@gmail.com>
2026-09-17 17:33:01 +0300

    refactor: edit fft recursive to iterative

--- internal/mathlib/fft.go
+++ internal/mathlib/fft.go
@@ -2,43 +2,58 @@ package mathlib
 
 import (
 	"math"
-	"math/cmplx"
 )
 
-func FFT(x []complex128) []complex128 {
-	n := len(x)
-
-	if n <= 1 {
-		return x
+func FFT(samples []float64) []complex128 {
+	n := len(samples)
+	if n == 0 {
+		return nil
 	}
 
+	spectrum := make([]complex128, n)
+	for i, sample := range samples {
+		spectrum[i] = complex(sample, 0)
+	}
+
+	if n == 1 {
+		return spectrum
+	}
 	if n&(n-1) != 0 {
-		return dft(x)
+		return dft(spectrum)
 	}
 
-	half := n / 2
-	even := make([]complex128, half)
-	odd := make([]complex128, half)
+	for i, j := 1, 0; i < n; i++ {
+		bit := n >> 1
+		for j&bit != 0 {
+			j ^= bit
+			bit >>= 1
+		}
+		j ^= bit
 
-	for i := 0; i < half; i++ {
-		even[i] = x[i*2]
-		odd[i] = x[i*2+1]
+		if i < j {
+			spectrum[i], spectrum[j] = spectrum[j], spectrum[i]
+		}
 	}
 
-	fftEven := FFT(even)
-	fftOdd := FFT(odd)
+	for stageSize := 2; stageSize <= n; stageSize <<= 1 {
+		halfStage := stageSize / 2
+		angle := -2.0 * math.Pi / float64(stageSize)
+		stageTwiddle := complex(math.Cos(angle), math.Sin(angle))
 
-	res := make([]complex128, n)
+		for start := 0; start < n; start += stageSize {
+			twiddle := complex(1, 0)
+			for offset := 0; offset < halfStage; offset++ {
+				even := spectrum[start+offset]
+				odd := spectrum[start+offset+halfStage] * twiddle
 
-	for k := 0; k < half; k++ {
-		angle := -2.0 * math.Pi * float64(k) / float64(n)
-		twiddle := cmplx.Rect(1, angle) * fftOdd[k]
-
-		res[k] = fftEven[k] + twiddle
-		res[k+half] = fftEven[k] - twiddle
+				spectrum[start+offset] = even + odd
+				spectrum[start+offset+halfStage] = even - odd
+				twiddle *= stageTwiddle
+			}
+		}
 	}
 
-	return res
+	return spectrum
 }
 
 func dft(x []complex128) []complex128 {
@@ -48,7 +63,7 @@ func dft(x []complex128) []complex128 {
 	for k := 0; k < n; k++ {
 		for sample, value := range x {
 			angle := -2.0 * math.Pi * float64(k*sample) / float64(n)
-			res[k] += value * cmplx.Rect(1, angle)
+			res[k] += value * complex(math.Cos(angle), math.Sin(angle))
 		}
 	}
 
